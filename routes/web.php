5ae1cd923fd2860fbc068146417dfd36de2def1f
George Boot <george@famboot.nl>
2020-09-24 09:46:49 +0200

    wip

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('get_declared_classes', function () {
+    dd(get_declared_classes());
+});
+
+Route::get('opcache_get_status', function () {
+    dd(opcache_get_status());
+});
+
+Route::get('dir', function () {
+    dd(__DIR__);
+});
